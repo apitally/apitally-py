1ebd588dbc6c5c752485a6344905fbce4db0057e
Simon Gurcke <simon@gurcke.de>
2023-07-17 18:50:57 +1000

    Send OpenAPI

--- starlette_apitally/app_info.py
+++ starlette_apitally/app_info.py
@@ -13,57 +13,25 @@ import starlette_apitally
 
 def get_app_info(app: ASGIApp, app_version: Optional[str], openapi_url: Optional[str]) -> Dict[str, Any]:
     app_info: Dict[str, Any] = {}
-    if schema := get_openapi_schema(app, openapi_url):
-        info = {}
-        if (title := schema["info"].get("title")) and title != "FastAPI":
-            info["title"] = title.strip()
-        if summary := schema["info"].get("summary"):
-            info["summary"] = summary.strip()
-        if description := schema["info"].get("description"):
-            info["description"] = description.strip()
-        if app_version:
-            info["version"] = app_version
-        elif version := schema["info"].get("version"):
-            info["version"] = version.strip()
-        if info:
-            app_info["info"] = info
-        app_info["paths"] = transform_openapi_paths(schema["paths"])
+    if openapi := get_openapi(app, openapi_url):
+        app_info["openapi"] = openapi
     elif endpoints := get_endpoint_info(app):
-        if app_version:
-            app_info["info"] = {"version": app_version}
         app_info["paths"] = [{"path": endpoint.path, "method": endpoint.http_method} for endpoint in endpoints]
+    app_info["versions"] = get_versions(app_version)
     app_info["client"] = "starlette-apitally"
-    app_info["versions"] = get_versions()
     return app_info
 
 
-def get_openapi_schema(app: ASGIApp, openapi_url: Optional[str]) -> Optional[Dict[str, Any]]:
+def get_openapi(app: ASGIApp, openapi_url: Optional[str]) -> Optional[str]:
     if not openapi_url:
         return None
     try:
         client = TestClient(app, raise_server_exceptions=False)
         response = client.get(openapi_url)
         response.raise_for_status()
-        return response.json()
+        return response.text
     except HTTPStatusError:
         return None
-
-
-def transform_openapi_paths(openapi_paths: Dict[str, Any]) -> List[Dict[str, Any]]:
-    paths: List[Dict[str, Any]] = []
-    for path, path_item in openapi_paths.items():
-        for method, operation in path_item.items():
-            item = {"path": path, "method": method}
-            if summary := operation.get("summary") or path_item.get("summary"):
-                item["summary"] = summary
-            if description := operation.get("description") or path_item.get("description"):
-                item["description"] = description
-            if responses := operation.get("responses"):
-                item["responses"] = {
-                    status_code: response.get("description", "") for status_code, response in responses.items()
-                }
-            paths.append(item)
-    return paths
 
 
 def get_endpoint_info(app: ASGIApp) -> List[EndpointInfo]:
@@ -80,7 +48,7 @@ def get_routes(app: ASGIApp) -> List[BaseRoute]:
     return []
 
 
-def get_versions() -> Dict[str, str]:
+def get_versions(app_version: Optional[str] = None) -> Dict[str, str]:
     versions = {
         "python": f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}",
         "starlette-apitally": starlette_apitally.__version__,
@@ -92,4 +60,6 @@ def get_versions() -> Dict[str, str]:
         versions["fastapi"] = fastapi.__version__
     except (ImportError, AttributeError):
         pass
+    if app_version:
+        versions["app"] = app_version
     return versions
--- tests/test_app_info.py
+++ tests/test_app_info.py
@@ -17,9 +17,9 @@ def test_get_app_info(app: Starlette, mocker: MockerFixture):
         app.middleware_stack = app.build_middleware_stack()
 
     app_info = get_app_info(app=app.middleware_stack, app_version=None, openapi_url="/openapi.json")
-    assert len(app_info["paths"]) == 4
+    assert ("paths" in app_info) != ("openapi" in app_info)  # only one, not both
 
     app_info = get_app_info(app=app.middleware_stack, app_version="1.2.3", openapi_url=None)
-    assert app_info["info"]["version"] == "1.2.3"
     assert len(app_info["paths"]) == 4
-    assert len(app_info["versions"]) > 0
+    assert len(app_info["versions"]) > 1
+    app_info["versions"]["app"] == "1.2.3"
