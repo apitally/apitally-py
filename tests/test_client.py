668be5dc01ddd8aad34fa2d4e319dd26af83d8d2
Simon Gurcke <simon@gurcke.de>
2023-08-15 11:28:47 +1000

    Fix tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import asyncio
 import json
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, AsyncIterator
 from unittest.mock import MagicMock
 
 import pytest
@@ -15,14 +15,17 @@ if TYPE_CHECKING:
 
 
 CLIENT_ID = "76b5cb91-a0a4-4ea0-a894-57d2b9fcb2c9"
+ENV = "default"
 
 
 @pytest.fixture()
-async def client() -> ApitallyClient:
+async def client(mocker: MockerFixture) -> AsyncIterator[ApitallyClient]:
     from starlette_apitally.client import ApitallyClient
 
-    client = ApitallyClient(client_id=CLIENT_ID, env="default", enable_keys=True)
-    client.stop_sync_loop()
+    mocker.patch("starlette_apitally.client.ApitallyClient.start_sync_loop")
+    mocker.patch("starlette_apitally.client.ApitallyClient._run_sync_loop")
+
+    client = ApitallyClient(client_id=CLIENT_ID, env=ENV, enable_keys=True)
     client.requests.log_request(
         method="GET",
         path="/test",
@@ -35,7 +38,7 @@ async def client() -> ApitallyClient:
         status_code=200,
         response_time=0.227,
     )
-    return client
+    yield client
 
 
 async def test_send_requests_data(client: ApitallyClient, httpx_mock: HTTPXMock):
@@ -45,7 +48,7 @@ async def test_send_requests_data(client: ApitallyClient, httpx_mock: HTTPXMock)
     async with client.get_http_client() as http_client:
         await client.send_requests_data(client=http_client)
 
-    requests = httpx_mock.get_requests(url=f"{HUB_BASE_URL}/{HUB_VERSION}/{CLIENT_ID}/default/requests")
+    requests = httpx_mock.get_requests(url=f"{HUB_BASE_URL}/{HUB_VERSION}/{CLIENT_ID}/{ENV}/requests")
     assert len(requests) == 1
     request_data = json.loads(requests[0].content)
     assert len(request_data["requests"]) == 1
@@ -62,7 +65,7 @@ async def test_send_app_info(client: ApitallyClient, httpx_mock: HTTPXMock, mock
     client.send_app_info(app=app_mock, app_version="1.2.3", openapi_url="/openapi.json")
     await asyncio.sleep(0.01)
 
-    requests = httpx_mock.get_requests(url=f"{HUB_BASE_URL}/{HUB_VERSION}/{CLIENT_ID}/default/info")
+    requests = httpx_mock.get_requests(url=f"{HUB_BASE_URL}/{HUB_VERSION}/{CLIENT_ID}/{ENV}/info")
     assert len(requests) == 1
     request_data = json.loads(requests[0].content)
     assert request_data["paths"] == []
@@ -76,6 +79,6 @@ async def test_get_keys(client: ApitallyClient, httpx_mock: HTTPXMock):
     await client.get_keys()
     await asyncio.sleep(0.01)
 
-    requests = httpx_mock.get_requests(url=f"{HUB_BASE_URL}/{HUB_VERSION}/{CLIENT_ID}/default/keys")
+    requests = httpx_mock.get_requests(url=f"{HUB_BASE_URL}/{HUB_VERSION}/{CLIENT_ID}/{ENV}/keys")
     assert len(requests) == 1
     assert len(client.keys.keys) == 1
