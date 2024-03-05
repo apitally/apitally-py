c52a34af472b5b94854dd7299b09063599118c12
Simon Gurcke <simon@gurcke.de>
2024-03-06 09:39:26 +1000

    Add support for Litestar (#13)
    * Add ApitallyPlugin for Litestar
    
    * Add identify_consumer_callback argument and fix validation error capture
    
    * Add tests
    
    * Fix test coverage
    
    * Update readme
    
    * Clean up
    
    * Fix typing
    
    * Add litestar to text matrix in CI
    
    * Move start_time definition down
    
    * Add filter_openapi_paths argument
    
    * Use contextlib.suppress
    
    * Improve typing for ApitallyClient
    
    * Fix getting correct path from Request object

---
+++ apitally/litestar.py
@@ -0,0 +1,186 @@
+import contextlib
+import json
+import sys
+import time
+from importlib.metadata import version
+from typing import Callable, Dict, List, Optional
+
+from litestar.app import DEFAULT_OPENAPI_CONFIG, Litestar
+from litestar.config.app import AppConfig
+from litestar.connection import Request
+from litestar.datastructures import Headers
+from litestar.enums import ScopeType
+from litestar.handlers import HTTPRouteHandler
+from litestar.plugins import InitPluginProtocol
+from litestar.types import ASGIApp, Message, Receive, Scope, Send
+
+from apitally.client.asyncio import ApitallyClient
+
+
+__all__ = ["ApitallyPlugin"]
+
+
+class ApitallyPlugin(InitPluginProtocol):
+    def __init__(
+        self,
+        client_id: str,
+        env: str = "dev",
+        app_version: Optional[str] = None,
+        filter_openapi_paths: bool = True,
+        identify_consumer_callback: Optional[Callable[[Request], Optional[str]]] = None,
+    ) -> None:
+        self.client = ApitallyClient(client_id=client_id, env=env)
+        self.app_version = app_version
+        self.filter_openapi_paths = filter_openapi_paths
+        self.identify_consumer_callback = identify_consumer_callback
+        self.openapi_path: Optional[str] = None
+
+    def on_app_init(self, app_config: AppConfig) -> AppConfig:
+        app_config.on_startup.append(self.on_startup)
+        app_config.middleware.append(self.middleware_factory)
+        return app_config
+
+    def on_startup(self, app: Litestar) -> None:
+        openapi_config = app.openapi_config or DEFAULT_OPENAPI_CONFIG
+        self.openapi_path = openapi_config.openapi_controller.path
+
+        app_info = {
+            "openapi": _get_openapi(app),
+            "paths": [route for route in _get_routes(app) if not self.filter_path(route["path"])],
+            "versions": _get_versions(self.app_version),
+            "client": "python:litestar",
+        }
+        self.client.set_app_info(app_info)
+        self.client.start_sync_loop()
+
+    def middleware_factory(self, app: ASGIApp) -> ASGIApp:
+        async def middleware(scope: Scope, receive: Receive, send: Send) -> None:
+            if scope["type"] == "http" and scope["method"] != "OPTIONS":
+                request = Request(scope)
+                response_status = 0
+                response_time = 0.0
+                response_headers = Headers()
+                response_body = b""
+                start_time = time.perf_counter()
+
+                async def send_wrapper(message: Message) -> None:
+                    nonlocal response_time, response_status, response_headers, response_body
+                    if message["type"] == "http.response.start":
+                        response_time = time.perf_counter() - start_time
+                        response_status = message["status"]
+                        response_headers = Headers(message["headers"])
+                    elif message["type"] == "http.response.body" and response_status == 400:
+                        response_body += message["body"]
+                    await send(message)
+
+                await app(scope, receive, send_wrapper)
+                self.add_request(
+                    request=request,
+                    response_status=response_status,
+                    response_time=response_time,
+                    response_headers=response_headers,
+                    response_body=response_body,
+                )
+            else:
+                await app(scope, receive, send)  # pragma: no cover
+
+        return middleware
+
+    def add_request(
+        self,
+        request: Request,
+        response_status: int,
+        response_time: float,
+        response_headers: Headers,
+        response_body: bytes,
+    ) -> None:
+        if response_status < 100 or not request.route_handler.paths:
+            return  # pragma: no cover
+        path = self.get_path(request)
+        if path is None or self.filter_path(path):
+            return
+        consumer = self.get_consumer(request)
+        self.client.request_counter.add_request(
+            consumer=consumer,
+            method=request.method,
+            path=path,
+            status_code=response_status,
+            response_time=response_time,
+            request_size=request.headers.get("Content-Length"),
+            response_size=response_headers.get("Content-Length"),
+        )
+        if response_status == 400 and response_body and len(response_body) < 4096:
+            with contextlib.suppress(json.JSONDecodeError):
+                parsed_body = json.loads(response_body)
+                if (
+                    isinstance(parsed_body, dict)
+                    and "detail" in parsed_body
+                    and isinstance(parsed_body["detail"], str)
+                    and "validation" in parsed_body["detail"].lower()
+                    and "extra" in parsed_body
+                    and isinstance(parsed_body["extra"], list)
+                ):
+                    self.client.validation_error_counter.add_validation_errors(
+                        consumer=consumer,
+                        method=request.method,
+                        path=path,
+                        detail=[
+                            {
+                                "loc": [error.get("source", "body")] + error["key"].split("."),
+                                "msg": error["message"],
+                                "type": "",
+                            }
+                            for error in parsed_body["extra"]
+                            if "key" in error and "message" in error
+                        ],
+                    )
+
+    def get_path(self, request: Request) -> Optional[str]:
+        path: List[str] = []
+        for layer in request.route_handler.ownership_layers:
+            if isinstance(layer, HTTPRouteHandler):
+                if len(layer.paths) == 0:
+                    return None  # pragma: no cover
+                path.append(list(layer.paths)[0].lstrip("/"))
+            else:
+                path.append(layer.path.lstrip("/"))
+        return "/" + "/".join(filter(None, path))
+
+    def filter_path(self, path: str) -> bool:
+        if self.filter_openapi_paths and self.openapi_path:
+            return path == self.openapi_path or path.startswith(self.openapi_path + "/")
+        return False  # pragma: no cover
+
+    def get_consumer(self, request: Request) -> Optional[str]:
+        if hasattr(request.state, "consumer_identifier"):
+            return str(request.state.consumer_identifier)
+        if self.identify_consumer_callback is not None:
+            consumer_identifier = self.identify_consumer_callback(request)
+            if consumer_identifier is not None:
+                return str(consumer_identifier)
+        return None
+
+
+def _get_openapi(app: Litestar) -> str:
+    schema = app.openapi_schema.to_schema()
+    return json.dumps(schema)
+
+
+def _get_routes(app: Litestar) -> List[Dict[str, str]]:
+    return [
+        {"method": method, "path": route.path}
+        for route in app.routes
+        for method in route.methods
+        if route.scope_type == ScopeType.HTTP and method != "OPTIONS"
+    ]
+
+
+def _get_versions(app_version: Optional[str]) -> Dict[str, str]:
+    versions = {
+        "python": f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}",
+        "apitally": version("apitally"),
+        "litestar": version("litestar"),
+    }
+    if app_version:
+        versions["app"] = app_version
+    return versions
---
+++ tests/test_litestar.py
@@ -0,0 +1,154 @@
+from __future__ import annotations
+
+from importlib.util import find_spec
+from typing import TYPE_CHECKING, Optional
+
+import pytest
+from pytest_mock import MockerFixture
+
+from .constants import CLIENT_ID, ENV
+
+
+if find_spec("litestar") is None:
+    pytest.skip("litestar is not available", allow_module_level=True)
+
+if TYPE_CHECKING:
+    from litestar.app import Litestar
+    from litestar.testing import TestClient
+
+
+@pytest.fixture(scope="module")
+async def app(module_mocker: MockerFixture) -> Litestar:
+    from litestar.app import Litestar
+    from litestar.connection import Request
+    from litestar.handlers import get, post
+
+    from apitally.litestar import ApitallyPlugin
+
+    module_mocker.patch("apitally.client.asyncio.ApitallyClient._instance", None)
+    module_mocker.patch("apitally.client.asyncio.ApitallyClient.start_sync_loop")
+    module_mocker.patch("apitally.client.asyncio.ApitallyClient.set_app_info")
+
+    @get("/foo")
+    async def foo() -> str:
+        return "foo"
+
+    @get("/foo/{bar:str}")
+    async def foo_bar(request: Request, bar: str) -> str:
+        request.state.consumer_identifier = "test2"
+        return f"foo: {bar}"
+
+    @post("/bar")
+    async def bar() -> str:
+        return "bar"
+
+    @post("/baz")
+    async def baz() -> None:
+        raise ValueError("baz")
+
+    @get("/val")
+    async def val(foo: int) -> str:
+        return "val"
+
+    def identify_consumer(request: Request) -> Optional[str]:
+        return "test1" if "/foo" in request.route_handler.paths else None
+
+    plugin = ApitallyPlugin(
+        client_id=CLIENT_ID,
+        env=ENV,
+        app_version="1.2.3",
+        identify_consumer_callback=identify_consumer,
+    )
+    app = Litestar(
+        route_handlers=[foo, foo_bar, bar, baz, val],
+        plugins=[plugin],
+    )
+    return app
+
+
+@pytest.fixture(scope="module")
+async def client(app: Litestar) -> TestClient:
+    from asgi_lifespan import LifespanManager
+    from litestar.testing import TestClient
+
+    async with LifespanManager(app):  # type: ignore[arg-type]
+        return TestClient(app)
+
+
+def test_middleware_requests_ok(client: TestClient, mocker: MockerFixture):
+    mock = mocker.patch("apitally.client.base.RequestCounter.add_request")
+
+    response = client.get("/foo/")
+    assert response.status_code == 200
+    mock.assert_called_once()
+    assert mock.call_args is not None
+    assert mock.call_args.kwargs["consumer"] == "test1"
+    assert mock.call_args.kwargs["method"] == "GET"
+    assert mock.call_args.kwargs["path"] == "/foo"
+    assert mock.call_args.kwargs["status_code"] == 200
+    assert mock.call_args.kwargs["response_time"] > 0
+
+    response = client.get("/foo/123")
+    assert response.status_code == 200
+    assert mock.call_count == 2
+    assert mock.call_args is not None
+    assert mock.call_args.kwargs["consumer"] == "test2"
+    assert mock.call_args.kwargs["path"] == "/foo/{bar:str}"
+
+    response = client.post("/bar")
+    assert response.status_code == 201
+    assert mock.call_count == 3
+    assert mock.call_args is not None
+    assert mock.call_args.kwargs["method"] == "POST"
+
+    response = client.get("/schema/openapi.json")
+    assert response.status_code == 200
+    assert mock.call_count == 3  # OpenAPI paths are filtered out
+
+
+def test_middleware_requests_unhandled(client: TestClient, mocker: MockerFixture):
+    mock = mocker.patch("apitally.client.base.RequestCounter.add_request")
+
+    response = client.post("/xxx")
+    assert response.status_code == 404
+    mock.assert_not_called()
+
+
+def test_middleware_requests_error(client: TestClient, mocker: MockerFixture):
+    mock = mocker.patch("apitally.client.base.RequestCounter.add_request")
+
+    response = client.post("/baz")
+    assert response.status_code == 500
+    mock.assert_called_once()
+    assert mock.call_args is not None
+    assert mock.call_args.kwargs["method"] == "POST"
+    assert mock.call_args.kwargs["path"] == "/baz"
+    assert mock.call_args.kwargs["status_code"] == 500
+    assert mock.call_args.kwargs["response_time"] > 0
+
+
+def test_middleware_validation_error(client: TestClient, mocker: MockerFixture):
+    mock = mocker.patch("apitally.client.base.ValidationErrorCounter.add_validation_errors")
+
+    # Validation error as foo must be an integer
+    response = client.get("/val?foo=bar")
+    assert response.status_code == 400
+
+    mock.assert_called_once()
+    assert mock.call_args is not None
+    assert mock.call_args.kwargs["method"] == "GET"
+    assert mock.call_args.kwargs["path"] == "/val"
+    assert len(mock.call_args.kwargs["detail"]) == 1
+    assert mock.call_args.kwargs["detail"][0]["loc"] == ["query", "foo"]
+
+
+def test_get_app_info(app: Litestar, mocker: MockerFixture):
+    mock = mocker.patch("apitally.client.asyncio.ApitallyClient.set_app_info")
+    app.on_startup[0](app)  # type: ignore[call-arg]
+    mock.assert_called_once()
+    app_info = mock.call_args.args[0]
+    assert len(app_info["openapi"]) > 0
+    assert len(app_info["paths"]) == 5
+    assert app_info["versions"]["litestar"]
+    assert app_info["versions"]["app"] == "1.2.3"
+    assert app_info["client"] == "python:litestar"
