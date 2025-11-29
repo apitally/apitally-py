e991e2880487de706a94f7ad62fac50d6747327f
Simon Gurcke <simon@gurcke.de>
2025-11-29 21:54:28 +1000

    Add psutil error handling

--- apitally/client/resources.py
+++ apitally/client/resources.py
@@ -8,12 +8,15 @@ _is_first_interval = True
 
 def get_cpu_memory_usage() -> Optional[Dict[str, Union[float, int]]]:
     global _is_first_interval
-    process = psutil.Process()
-    data = {
-        "cpu_percent": process.cpu_percent(),
-        "memory_rss": process.memory_info().rss,
-    }
-    if _is_first_interval:
-        _is_first_interval = False
+    try:
+        process = psutil.Process()
+        data = {
+            "cpu_percent": process.cpu_percent(),
+            "memory_rss": process.memory_info().rss,
+        }
+        if _is_first_interval:
+            _is_first_interval = False
+            return None
+        return data
+    except psutil.Error:
         return None
-    return data
