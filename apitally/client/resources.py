8368e216679f22f1038027bbb39f35ecf5026574
Simon Gurcke <simon@gurcke.de>
2025-11-30 22:24:28 +1000

    Capture CPU and memory metrics (#194)
    * Capture CPU/memory metrics
    
    * Add psutil error handling
    
    * Fix

---
+++ apitally/client/resources.py
@@ -0,0 +1,22 @@
+from typing import Dict, Optional, Union
+
+import psutil
+
+
+_is_first_interval = True
+
+
+def get_cpu_memory_usage() -> Optional[Dict[str, Union[float, int]]]:
+    global _is_first_interval
+    try:
+        process = psutil.Process()
+        data = {
+            "cpu_percent": process.cpu_percent(),
+            "memory_rss": process.memory_info().rss,
+        }
+        if _is_first_interval:
+            _is_first_interval = False
+            return None
+        return data
+    except psutil.Error:  # pragma: no cover
+        return None
