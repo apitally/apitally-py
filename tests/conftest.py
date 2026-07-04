512af1cb70cc4cbbaaecb6363e3616721b6b5044
Simon Gurcke <simon@gurcke.de>
2026-07-05 00:34:03 +1000

    U4: Add providers and mode detection

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,17 @@
 import pytest
+from opentelemetry.test.globals_test import reset_trace_globals
 
-from apitally.shared import config
+from apitally.shared import config, providers
 
 
 @pytest.fixture(autouse=True)
 def reset_apitally_config():
     yield
     config.reset()
+
+
+@pytest.fixture(autouse=True)
+def reset_otel_trace_globals():
+    yield
+    reset_trace_globals()
+    providers.reset()
